10a3a495eab7ec280bbb4f1158c050993dc43562
hucce <huccce@gmail.com>
2020-08-26 17:00:00 +0900

    no message

---
+++ Assets/Flappy Bird Style/Scripts/Bird.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour
+{
+    public float upForce;                   //Upward force of the "flap".
+    //private bool isDead = false;            //Has the player collided with a wall?
+
+    private Animator anim;                  //Reference to the Animator component.
+    private Rigidbody2D rb2d;               //Holds a reference to the Rigidbody2D component of the bird.
+
+    private void Start()
+    {
+        //Get reference to the Animator component attached to this GameObject.
+        anim = GetComponent<Animator>();
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+}
---
+++ Assets/Flappy Bird Style/Scripts/GameControl.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;         //A reference to our game control script so we can access it statically.
+    public GameObject mainCamera;
+    private GameObject mainObjCamera;
+    public bool playerPlay = false;
+    public bool record = false;
+    public bool resetColumns = false;
+    public bool swapNNModel = false;
+    public int minRecordGame = 2;
+    public int maxRecordGame = 12;
+    public int maxTrainNum = 20;
+    public string folderName = "";
+
+    private void Awake()
+    {
+        maxRecordGame = minRecordGame + maxRecordGame;
+
+        if (record)
+        {
+            QualitySettings.vSyncCount = 0; // vsync 사용안함
+            Application.targetFrameRate = 30;
+        }
+
+        //If we don't currently have a game control...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if (instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        GameObject[] brids = GameObject.FindGameObjectsWithTag("BirdAgentScean");
+        for (int i = 0; i < brids.Length; i++)
+        {
+            if (brids[i].GetComponent<BirdAgentScean>().mainScean)
+            {
+                mainObjCamera = brids[i];
+                break;
+            }
+        }
+        mainObjCamera = mainObjCamera.transform.GetComponentInChildren<Camera>().gameObject;
+    }
+
+    private void LateUpdate()
+    {
+        mainCamera.transform.position = new Vector3(mainObjCamera.transform.position.x, mainObjCamera.transform.position.y, mainObjCamera.transform.position.z);
+    }
+
+    public void AppExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
